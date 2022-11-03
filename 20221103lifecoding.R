4e3bb3c61b8e7262ff1a2774ef60ca47701ba096
Claus Rüffler <claus.rueffler@posteo.net>
2022-11-03 15:19:53 +0100

    open link to demograhic stochasticity tab

---
+++ 20221103lifecoding.R
@@ -0,0 +1,21 @@
+Nmax <- 20
+N <- 10
+time.series <- c(N)
+
+b <- 1.1
+d <- 1
+Dt <- 0.001
+
+stochastic.time.series <- function(N, Nmax, b, d, Dt){
+  time.series <- c(N)
+  while (N > 0 & N < Nmax) {
+  event <- sample(c(1, -1, 0), 1, c(b*N*Dt, d*N*Dt, 1-N*Dt*(b + d)), replace = TRUE)
+  N <- N + event
+  time.series <- append(time.series, N)
+  }
+  return(time.series)
+}
+
+result <- stochastic.time.series(10, 20, 1.0, 1.1, 0.001)
+
+plot(result)
