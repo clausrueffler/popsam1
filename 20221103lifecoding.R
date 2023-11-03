95bade44287a3483d5090368b7da9ea3e4173efb
Claus Rüffler <claus.rueffler@posteo.net>
2023-11-03 16:29:12 +0100

    updates life coding files

--- 20221103lifecoding.R
+++ 20221103lifecoding.R
@@ -1,36 +1,14 @@
-Nmax <- 20
-N <- 10
-time.series <- c(N)
-
-
+# Method 1: more elegant?
 stochastic.time.series <- function(N, Nmax, b, d, Dt){
   time.series <- c(N)
   while (N > 0 & N < Nmax) {
-  event <- sample(c(1, -1, 0), 1, c(b*N*Dt, d*N*Dt, 1-N*Dt*(b + d)), replace = TRUE)
-  N <- N + event
-  time.series <- append(time.series, N)
+    event <- sample(c(1, -1, 0), 1, c(b*N*Dt, d*N*Dt, 1-N*Dt*(b + d)), replace = TRUE)
+    N <- N + event
+    time.series <- append(time.series, N)
   }
   return(time.series)
 }
-
 result <- stochastic.time.series(10, 20, 1.0, 1.1, 0.001)
-
-plot(result, type = "l", xlab = "time", ylab = "population size")
-
-
-# Method 1: more elegant?
-stochastic.time.series <- function(N0, Nmax, b, d, Dt){
-  n <- c(N0)
-  t <- 1
-  while (n[t] > 0 & n[t] < Nmax) {
-    event <- sample(c(1, -1, 0), 1, c(b*n[t]*Dt, d*n[t]*Dt, 1-n[t]*Dt*(b + d)), replace = TRUE)
-    n[t+1] <- n[t] + event
-    t <- t + 1
-  }
-  return(n)
-}
-result <- stochastic.time.series(10, 20, 1.0, 1.1, 0.01)
-result
 plot(result, type = "l", xlab = "time", ylab = "population size")
 
 # Method 2: more intuitive?
@@ -45,7 +23,6 @@ stochastic.time.series <- function(N0, Nmax, b, d, Dt){
   return(n)
 }
 result <- stochastic.time.series(10, 20, 1.0, 1.1, 0.01)
-result
 plot(result, type = "l", xlab = "time", ylab = "population size")
 
 
