56589efcdc2686192318df930b99711cc452c04f
Claus Rüffler <claus.rueffler@posteo.net>
2023-11-03 16:20:10 +0100

    updates

--- 20221103lifecoding.R
+++ 20221103lifecoding.R
@@ -2,9 +2,6 @@ Nmax <- 20
 N <- 10
 time.series <- c(N)
 
-b <- 1.1
-d <- 1
-Dt <- 0.001
 
 stochastic.time.series <- function(N, Nmax, b, d, Dt){
   time.series <- c(N)
@@ -21,4 +18,43 @@ result <- stochastic.time.series(10, 20, 1.0, 1.1, 0.001)
 plot(result, type = "l", xlab = "time", ylab = "population size")
 
 
-rexp(1, rate = 2)
+# Method 1: more elegant?
+stochastic.time.series <- function(N0, Nmax, b, d, Dt){
+  n <- c(N0)
+  t <- 1
+  while (n[t] > 0 & n[t] < Nmax) {
+    event <- sample(c(1, -1, 0), 1, c(b*n[t]*Dt, d*n[t]*Dt, 1-n[t]*Dt*(b + d)), replace = TRUE)
+    n[t+1] <- n[t] + event
+    t <- t + 1
+  }
+  return(n)
+}
+result <- stochastic.time.series(10, 20, 1.0, 1.1, 0.01)
+result
+plot(result, type = "l", xlab = "time", ylab = "population size")
+
+# Method 2: more intuitive?
+stochastic.time.series <- function(N0, Nmax, b, d, Dt){
+  n <- c(N0)
+  t <- 1
+  while (n[t] > 0 & n[t] < Nmax) {
+    event <- sample(c(1, -1, 0), 1, c(b*n[t]*Dt, d*n[t]*Dt, 1-n[t]*Dt*(b + d)), replace = TRUE)
+    n[t+1] <- n[t] + event
+    t <- t + 1
+  }
+  return(n)
+}
+result <- stochastic.time.series(10, 20, 1.0, 1.1, 0.01)
+result
+plot(result, type = "l", xlab = "time", ylab = "population size")
+
+
+# drawing waiting times from exponential distribution
+
+rexp(5, rate = 2)
+
+
+
+
+
+
