711ab4b27c01725a7999976678a71346498ed142
Claus Rüffler <claus.rueffler@posteo.net>
2023-11-07 20:30:38 +0100

    update Allee

--- Bifurcation_Allee.R
+++ Bifurcation_Allee.R
@@ -13,7 +13,7 @@ abline(v=0)
 # (N-values where the rate of change equals zero).
 # For the above parameter values this is indeed the case as can be seen by the graph.
 
-# N=0 is the trival equilibrium.
+# N=0 is the trivial equilibrium.
 # The other two equilibrium values can be found by solving the expression in brackets in the ODE for N=0.
 # 0=c*N*b - c*a*N^2 - d
 # The results is given by the following two functions:
@@ -37,5 +37,12 @@ curve(n2equil(a=0.1, x, c=0.2, d=0.3), n = 20000, from = 0.0, to = 3, lty = 2, y
 curve(n3equil(a=0.1, x, c=0.2, d=0.3), n = 20000, from = 0.0, to = 3, add=TRUE)
 curve(0*x, from = 0, to= 3, add=TRUE)
 
+# Note that the parameter n in the plotting command is NOT the population size but the "resolution" of the plot. It tells R at
+# how many different values to evaluate the function in order to draw the graph.
+
 # In this plot, the two solid lines show stable equilibria and the hatched line shows the unstable (= repelling) equilibrium
-      
+    
+# Next comes a bifurcation diagram where we vary a, that is, the sensitivity to crowding.
+curve(n2equil(x, b=1, c=0.2, d=0.3), n = 20000, from = 0.0, to = 0.3, lty = 2, ylim = c(0, 50), xlab = "Sensitivity to crowding", ylab = "Equilibrium population size, N")
+curve(n3equil(x, b=1, c=0.2, d=0.3), n = 20000, from = 0.0, to = 0.3, add=TRUE)
+curve(0*x, from = 0, to= 3, add=TRUE)
